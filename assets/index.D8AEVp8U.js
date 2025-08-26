c78991e4200f601ec38b45ec94c37b5302ebcb1e
18005036 <gekk@haier.com>
2025-08-26 16:58:41 +0800

    支持股票分组同步

--- assets/index.D8AEVp8U.js
+++
@@ -1,1 +0,0 @@
-import{F as o}from"./index.DZnAbtg5.js";import"./vue.Bq5FSDyb.js";export{o as default};
---
+++ assets/log.NTHpprwT.js
@@ -0,0 +1,1 @@
+import{s as e}from"./index.XaB_BWRs.js";const o=a=>e({url:"/admin/log/page",method:"get",params:a}),t=a=>e({url:"/admin/log",method:"delete",data:a});export{t as d,o as p};
